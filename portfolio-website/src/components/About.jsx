2f0bf30115d9179cdb553240185a5a4fc676e730
Abhijit <paulabhijit2607@gmail.com>
2025-02-03 04:14:29 +0530

    About Section
    Completed the About section. More work
    can be done on responsive and design.

--- portfolio-website/src/components/About.jsx
+++ portfolio-website/src/components/About.jsx
@@ -6,21 +6,21 @@ import future_interns from '../assets/icons/future_interns_logo.jpg';
 const About = () => {
   return (
     <Section id='about' className='space-y-15 text-sm'>
-      <Section >
+      <Section className='lg:flex lg:items-center lg:justify-start'>
         <div className="flex">
-          <img className=' h-25 w-auto bg-[url("/assets/gradient/gradient.svg")] bg-no-repeat bg-cover border-1 border-[#80808036] rounded-2xl' src={displayPicture} alt="Display Picture of Abhijit Paul." />
+          <img className='lg:h-70 h-25 bg-[url("/assets/gradient/gradient.svg")] bg-no-repeat bg-cover border-1 border-[#80808036] rounded-2xl' src={displayPicture} alt="Display Picture of Abhijit Paul." />
           <div className="flex">
             <img className='h-15' src="/assets/Arrow.svg" alt="Arrow Icon" />
-            <span>Hello! I Am <p className='text-themed-text'>Abhijit Paul</p></span>
+            <span className='whitespace-nowrap'>Hello! I Am <span className='text-themed-text'>Abhijit paul</span></span>
           </div>
         </div>
-        <div className="w-2/3 mx-auto mt-4 text-xs">
+        <div className="w-2/3 mx-auto mt-4 text-xs lg:text-sm">
           <p>A Developer who</p>
           <span className='text-4xl'>Judges a business by its <span className="text-themed-text">landing page</span>...</span>
           <p>Because if the landing page doesn't impress you than what can?</p>
         </div>
       </Section>
-      <Section className='text-sm'>
+      <Section className='text-sm lg:text-lg'>
         <h1 className='text-4xl'>I'm a Full Stack Developer.|</h1>
         <span>Currently, I'm an Intern at
           <span className='text-green-500'>
@@ -30,7 +30,7 @@ const About = () => {
         </span>
       </Section>
       <Section>
-        <p className='text-left'>
+        <p className='text-left lg:text-lg'>
           Hello, I'm Abhijit Paul, a passionate self-taught full stack web developer dedicated to revolutionizing the digital world with creative and impactful solutions. Let's build the future together!
         </p>
       </Section>
