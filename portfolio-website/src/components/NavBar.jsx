c1c239925359337a082d17786f15357f2cc53fb4
Abhijit <paulabhijit2607@gmail.com>
2025-02-18 09:56:33 +0530

    Scroll Navigation
    Added scroll into view navigation in the
    Navigation Bar. Though minor UI bugs are
    there to be fixed, like, carousel and c-
    ard elements are laying on top of NavBar
    and mobile view NavBar not closing when
    navigated to selected section.

--- portfolio-website/src/components/NavBar.jsx
+++ portfolio-website/src/components/NavBar.jsx
@@ -2,21 +2,29 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Section } from '@radix-ui/themes';
 
+
+// Navigation
+const handleNavigation = (e, section, setIsMenuClosed) => {
+  e.preventDefault();
+  document.getElementById(section).scrollIntoView({ behavior: 'smooth', block: 'start' });
+  if(setIsMenuClosed) setIsMenuClosed(true);  
+}
+
 // Shared Logo Component
 const NavLogo = () => (
   <img className='h-15' src="/Logo.svg" alt="Logo" />
 );
 
 // Shared Navigation Links Component
-const NavLinks = ({ mobile = false }) => (
-  <ul className={`${mobile ? 
-    'h-full space-y-10 text-3xl font-bold flex flex-col justify-center items-center' : 
+const NavLinks = ({ mobile = false }, setIsMenuClosed) => (
+  <ul className={`${mobile ?
+    'h-full space-y-10 text-3xl font-bold flex flex-col justify-center items-center' :
     'flex gap-10 *:hover:underline underline-offset-4'}`}
   >
-    <li><Link to='#about'>ABOUT</Link></li>
-    <li><Link to='#projects'>PROJECTS</Link></li>
-    <li><Link to='#skills'>SKILLS</Link></li>
-    <li><Link to='#contact'>CONTACT</Link></li>
+    <li><Link to={'/about'} onClick={(e) => handleNavigation(e, 'about', setIsMenuClosed)}>ABOUT</Link></li>
+    <li><Link to={'/projects'} onClick={(e) => handleNavigation(e, 'projects', setIsMenuClosed)}>PROJECTS</Link></li>
+    <li><Link to={'/skills'} onClick={(e) => handleNavigation(e, 'skills', setIsMenuClosed)}>SKILLS</Link></li>
+    <li><Link to={'/contact'} onClick={(e) => handleNavigation(e, 'contact', setIsMenuClosed)}>CONTACT</Link></li>
   </ul>
 );
 
@@ -24,12 +32,12 @@ const NavBar = () => {
   const [isMenuClosed, setIsMenuClosed] = useState(true);
 
   return (
-    <nav className='sticky bg-navbar'>
+    <nav className='sticky bg-navbar z-50'>
       {/* Mobile Controls */}
       <div className='md:hidden'>
         <Section className='h-15 p-5 flex justify-between items-center'>
           <NavLogo />
-          <Button 
+          <Button
             onClick={() => setIsMenuClosed(!isMenuClosed)}
             className='p-1 underline decoration-gray-600 underline-offset-3'
             variant='outline'
@@ -37,11 +45,11 @@ const NavBar = () => {
             Menu
           </Button>
         </Section>
-        
+
         {/* Mobile Menu */}
         <Section className={`${isMenuClosed ? 'hidden' : 'fixed inset-0 top-15 z-50 h-[calc(100vh-3.75rem)]'}`}>
           <div className="absolute inset-0 bg-navbar">
-            <NavLinks mobile />
+            <NavLinks mobile setIsMenuClosed={setIsMenuClosed}  />
           </div>
         </Section>
       </div>
