988d0125dfdac4608e49c3aff41dab025dfac37a
Abhijit <paulabhijit2607@gmail.com>
2025-02-23 04:18:53 +0530

    Created a separate backend branch.

--- client/postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import ('post-load-config').Config} */
-const config = {
-    plugins: {
-        "@tailwindcss/postcss": {},
-    },
-};
-
-export default config;
--- client/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export {default as NavBar} from './NavBar';
-export {default as About} from './About';
-export {default as Projects} from './Projects';
-export {default as Skills} from './Skills';
-export {default as Contact} from './Contact';
-export {default as EmailAlert} from './EmailAlert';
